90093852bf80f07eb4a21fe9e10b84762d21f0e8
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-09 11:19:12 +0700

    Add files via upload

---
+++ bagian 2 commit1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
---
+++ bagian_2 commit 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b)
+{
+    return (a + b) / 2;
+}
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
+
+int main()
+{
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+
+    status = Status_lulus(rerata);
+
+    cout << "Nilai rata-rata = " << rerata << endl;
+    cout << "Status kelulusan = " << status << endl;
+
+    return 0;
+}
